7fb18f13857ece34b1866902f1c4783ad5f6df83
Adenier Osorto <adenierosorto@gmail.com>
2021-11-05 19:14:18 -0600

    Delete de un municipio

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,19 +11,35 @@ export const  newMunicipio  = async (req, res) => {
     
     // construir el query 
     // consultas preparadas
-    cnn.query('INSERT INTO municipio SET ?', data, (err, result)=>{
+    cnn.query('INSERT INTO municipios SET ?', data, (err, result)=>{
         if(err){
             console.log(`ocurrio un error al insertar el registro, ${err}`);
+            res.redirect('/newMunicipio');
             return;
         }
-        res.redirect('/')
+        res.redirect('/newMunicipio');
     });
 }
 export const  selectMunicipio = async (req, res, next) => {  
     //contruir la data que sera insertada;
     const data = {};
-    cnn.query('SELECT * FROM municipio', data, (err, result)=>{
+    cnn.query('SELECT * FROM municipios', data, (err, result)=>{
+        if(err){
+            console.log(`Un error al mostrar la información: ${err}`);
+            return;
+        }
         req.data = result;
+        next();
+    });
+}
+export const  deleteMunicipio = async (req, res, next) => {  
+    const {id} = req.params;
+    cnn.query('DELETE FROM municipios WHERE municipioId = ?', [id], (err, result)=>{
+        if(err){
+            console.log(`Un error al eliminar la información: ${err}`);
+            return;
+        }
+        res.redirect('/newMunicipio');
         next();
     });
 }
--- routes/municipio.js
+++ routes/municipio.js
@@ -1,15 +1,19 @@
 import express from 'express';
-import {newMunicipio, selectMunicipio} from '../controllers/authController.js'
+import {deleteMunicipio, newMunicipio, selectMunicipio} from '../controllers/authController.js'
 const router = express.Router();
 
 
 router.get('/newMunicipio',selectMunicipio,(req, res) =>{
-    console.log(req.data);
     res.render('newMunicipio', {datos: req.data});
 });
+
+
 
 // insert
 router.post('/newMunicipio', newMunicipio);
 
 
+//1.25.00
+//delete
+router.get('/deleteMunicipio/:id', deleteMunicipio);
 export default router;
