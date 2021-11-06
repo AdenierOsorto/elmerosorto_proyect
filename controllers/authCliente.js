ed367ffbe3a397b7758f5fdfd3eb655accaaa232
Adenier Osorto <adenierosorto@gmail.com>
2021-11-06 13:24:29 -0600

    Insert de la tabla Clientes, cargar información en cbox

--- controllers/authCliente.js
+++ controllers/authCliente.js
@@ -1,19 +1,39 @@
 import cnn from '../database/connection.js';
-export const  newCliente  = async (req, res) => {
-    const { nommunicipio} = req.body;
-        //contruir la data que sera insertada;
-    const data = {
-        nombreMunicipio: nommunicipio
-    };
-    
-    // construir el query 
-    // consultas preparadas
-    cnn.query('INSERT INTO municipios SET ?', data, (err, result)=>{
+export const  selectCliente = async (req, res, next) => {  
+    //contruir la data que sera insertada;
+    const data = {};
+    cnn.query('SELECT * FROM municipios', data, (err, result)=>{
         if(err){
-            console.log(`ocurrio un error al insertar el registro, ${err}`);
-            res.redirect('/newMunicipio');
+            console.log(`Un error al mostrar la información: ${err}`);
             return;
         }
-        res.redirect('/newMunicipio');
+        req.data = result;
+        next();
+    });
+}
+export const  newCliente = async (req, res, next) => {  
+    //contruir la data que sera insertada;
+    console.log(req.body);
+    const {clienteCod, nombreCliente, 
+            apellidoCliente, emailCliente,
+            telefonoCliente, fechaCliente,
+            municipioCliente} = req.body;
+    const data = {
+        clienteCod: clienteCod,
+        nombre: nombreCliente,
+        apellido: apellidoCliente,
+        email: emailCliente,
+        telefono: telefonoCliente,
+        fechaNacimiento: fechaCliente,
+        municipioId:municipioCliente
+    };
+    cnn.query('INSERT INTO clientes SET ?', data, (err, result)=>{
+        if(err){
+            console.log(`Un error al mostrar la información: ${err}`);
+            res.redirect('/cliente');
+            return;
+        }
+        res.redirect('/cliente');
+        next();
     });
 }
--- routes/cliente.js
+++ routes/cliente.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import { newCliente } from '../controllers/authCliente.js';
+import { selectMunicipio } from '../controllers/authController.js';
 const router = express.Router();
 
-router.get('/cliente',(req, res) =>{
-    res.render('cliente');
+router.get('/cliente',selectMunicipio,(req, res) =>{
+    res.render('cliente',{munis:req.data});
 });
 
+router.post('/newCliente', newCliente);
 
 export default router;
